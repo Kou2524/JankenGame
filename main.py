a3483ce554bd4620b1cb36c10730a22df6afef01
Kou2524 <yasumaruyamu@gmail.com>
2025-11-12 16:04:45 +0900

    main.py を更新

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import pyxel
 
-pyxel.init(160, 120, title="じゃんけんゲーム")
+pyxel.init(160, 120, title="Janken Game")
+
+# じゃんけん画像を後で使いたい場合はここで pyxel.load("assets.pyxres") とかにできる
+# 今はテキストだけの仮タイトル画面
 
 def update():
     pass
 
 def draw():
     pyxel.cls(0)
-    # ここでのタイトル文字描画はやめて、HTML側で日本語を表示
+    pyxel.text(40, 50, "JANKEN GAME", 7)
     pyxel.text(30, 80, "PRESS ANY KEY...", 13)
 
 pyxel.run(update, draw)
